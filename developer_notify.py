643f4a5b4b9be29a5fd9c62147a69abab5b16c6a
Harp B <harp.bola@getsocialize.com>
2012-02-24 14:42:35 -0800

    Updated gitignore

--- developer_notify.py
+++ developer_notify.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python
-import os, time
+from settings import *
+import simplejson, sys
+from oauth_hook import OAuthHook
+import requests
+from defs import log
 
-SCRIPT_PATH = os.path.realpath(os.path.dirname(__file__))
-FILE_PATH = '%s/developer-notify-out.txt' % SCRIPT_PATH
-print FILE_PATH
-f = open(FILE_PATH,'w')
-out = 'Hi from Harp @ %s' % time.time()
-print out
-print >> f, out
+log( 'sys.argv: %s' % sys.argv )
+    
+# create consumer and token
+OAuthHook.consumer_key = CONSUMER_KEY
+OAuthHook.consumer_secret = CONSUMER_SECRET
+oauth_hook = OAuthHook('', '', header_auth=True)
+
+# do it
+url = "%s/application/%d/notification/" % (PARTNER_API_BASE, APPLICATION_ID)
+data = {"message" : "Harp was here."}
+body = {'payload' : simplejson.dumps(data) }
+
+client = requests.session(hooks={'pre_request': oauth_hook})
+response = client.post(url, body)
+log('Response status: %s' % response.status_code)
+log('Response content: %s' % response.content)
