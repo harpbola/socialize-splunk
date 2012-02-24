80ee80da7511abf52547ea5fc9de9131deb47266
Harp B <harp.bola@getsocialize.com>
2012-02-24 15:39:10 -0800

    format sys argv

--- developer_notify.py
+++ developer_notify.py
@@ -1,3 +1,4 @@
+#!/opt/splunk/bin/scripts/socialize-splunk/pyenv/bin/python
 from settings import *
 import simplejson, sys
 from oauth_hook import OAuthHook
@@ -5,6 +6,20 @@ import requests
 from defs import log
 
 log( 'sys.argv: %s' % sys.argv )
+try:
+    number_of_events = sys.argv[1]
+    search_terms = sys.argv[2]
+    alert_title = sys.argv[4]
+    alert_reason = sys.argv[5]
+    
+    log(  '''
+        number_of_events : %s
+        search_terms : %s
+        alert_title : %s
+        alert_reason : %s
+    ''' % (number_of_events, search_terms, alert_title, alert_reason) )
+except IndexError:
+    pass
     
 # create consumer and token
 OAuthHook.consumer_key = CONSUMER_KEY
