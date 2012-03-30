f9ea528d1f2888263214dcc9ae005d7f5a0111ec
Harp B <hil@harpb.com>
2012-03-30 14:32:24 -0700

    modularized

--- developer_notify.py
+++ developer_notify.py
@@ -3,40 +3,43 @@ import sys
 from defs import log
 from helper import post_comment, read_search
 
-log( 'sys.argv: %s' % sys.argv )
-saved_search_title = 'script-called-directly'
-alert_reason = 'script-called-directly'
-try:
-    number_of_events = sys.argv[1]
-    search_terms = sys.argv[2]
-    saved_search_title = sys.argv[4]
-    alert_reason = sys.argv[5]
-    search_url = sys.argv[6]
-    search_file_path = sys.argv[8]
+def argv_to_dict(argv):
+    try:
+        return dict(number_of_events = argv[1],
+            search_terms = argv[2],
+            saved_search_title = argv[4],
+            alert_reason = argv[5],
+            search_url = argv[6],
+            search_file_path = argv[8]
+        )
+    except IndexError:
+        return {}
+              
+def handle_search(kwargs):
+    log( 'kwargs: %s' % kwargs)
+    saved_search_title = kwargs.get('saved_search_title') if kwargs.get('saved_search_title') else 'script-called-directly'
+    alert_reason = 'script-called-directly'
     
-    log(  '''
-        number_of_events : '%(number_of_events)s'
-        search_terms : '%(search_terms)s'
-        saved_search_title : '%(saved_search_title)s'
-        alert_reason : '%(alert_reason)s'
-        search_url : '%(search_url)s'
-        search_file_path : '%(search_file_path)s'
-    ''' % locals() )
-except IndexError:
-    pass
-    
-# Create message
-entity_key = "socialize-status-main"
-if saved_search_title == '[PROD] Error - Internal Server - 5 min':
-    message = '%s ERRORS in last 5 mins.' % (number_of_events)
-elif saved_search_title == '[PROD] Access - 1 hour':
-    message = 'Only %s requests in last hour. Kinda slow.' % (number_of_events)
-elif saved_search_title == 'Summary -- HTTP by minute':
-    entity_key = 'http-requests'
-    search_data = read_search(search_file_path)
-    log( "search_data: %s" % search_data)
-    message = str(search_data[0])
-else:
-    message = alert_reason
-
-post_comment(entity_key, message)
+    # Create message
+    entity_key = "socialize-status-main"
+    if saved_search_title == '[PROD] Error - Internal Server - 5 min':
+        message = '%s ERRORS in last 5 mins.' % ( kwargs['number_of_events'])
+        
+    elif saved_search_title == 'Summary -- HTTP by Hour':
+        entity_key = 'http-requests'
+        search_data = read_search(kwargs['search_file_path'])
+        log( "search_data: %s" % search_data)
+        message = "Avg. Response Time: %(average_reponse_time)s \n"\
+            "Total Requests: %(request_count)s\n"\
+            "Oauth Errors: %(failure_count)s\n"\
+            "Server Errors: %(error_count)s\n"\
+            % search_data[0]
+        post_comment(entity_key, message)
+        
+    else:
+        message = alert_reason
+        post_comment(entity_key, message)
+        
+if __name__ == "__main__":
+    argv = argv_to_dict(sys.argv)
+    handle_search(argv)
---
+++ developer_notify_tests.py
@@ -0,0 +1,30 @@
+from developer_notify import argv_to_dict, handle_search
+
+sample_local_argv = [
+    '/opt/splunk/bin/scripts/notify.py', 
+    '1', 
+    'source="/mnt/logs/nginx-prod.log" | rex "\\:00 (?P<host_ip>[^\\s]+) " | rex field=UserAgent "^(?P<platform>[^-]+)-(?P<platform_version>[^\\/]+)\\/(?P<device_name>.+)\\sSocializeSDK" | stats avg(ResponseTime) AS "average_reponse_time", p80(ResponseTime) AS "p80_reponse_time", count AS "request_count", count(eval(ResponseCode<400)) AS "success_count", count(eval(ResponseCode>=400)) AS "failure_count", count(eval(ResponseCode>=500)) AS "error_count", count(eval(platform="Android")) AS "android_count", count(eval(platform="iOS")) AS "ios_count"', 'source="/mnt/logs/nginx-prod.log" | rex "\\:00 (?P<host_ip>[^\\s]+) " | rex field=UserAgent "^(?P<platform>[^-]+)-(?P<platform_version>[^\\/]+)\\/(?P<device_name>.+)\\sSocializeSDK" | stats avg(ResponseTime) AS "average_reponse_time", p80(ResponseTime) AS "p80_reponse_time", count AS "request_count", count(eval(ResponseCode<400)) AS "success_count", count(eval(ResponseCode>=400)) AS "failure_count", count(eval(ResponseCode>=500)) AS "error_count", count(eval(platform="Android")) AS "android_count", count(eval(platform="iOS")) AS "ios_count"', 
+    'Summary -- HTTP by Hour', 
+    'Saved Search [Summary -- HTTP by minute] always(1)', 
+    'http://logs.getsocialize.com:8000/app/search/@go?sid=scheduler_c3BsdW5rX2JvdA__search_U3VtbWFyeSAtLSBIVFRQIGJ5IG1pbnV0ZQ_at_1333140960_0e62db912459d27b', '', 
+    'sample_search_result/results.csv.gz'
+]
+ 
+
+#===============================================================================
+# argv_to_dict
+#===============================================================================
+def test_argv_to_dict():
+    print argv_to_dict(sample_local_argv)
+    
+#test_argv_to_dict()
+
+
+#===============================================================================
+# handle_search
+#===============================================================================
+def test_handle_search():
+    argv = argv_to_dict(sample_local_argv)
+    handle_search(argv)
+    
+test_handle_search()
